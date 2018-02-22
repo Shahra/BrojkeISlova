ba0bae3838f9097b5374074afa2295081d09b0ad
Zeljko Djuric <zdjuric@yandex.com>
2018-02-22 23:32:23 +0100

    Added MainMenuView

---
+++ BrojkeISlova/BrojkeISlova/MainMenuView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrojkeISlova {
+  public partial class MainMenuView : Form {
+    public MainMenuView() {
+      InitializeComponent();
+      //FormBorderStyle = FormBorderStyle.None;
+      //WindowState = FormWindowState.Maximized;
+    }
+
+    private void MainMenuView_Load(object sender, EventArgs e) {
+      UtilityFunctions.PositionButton(igrajButton, 0.2, 0.2, 0.6, 0.2, ClientSize);
+      UtilityFunctions.PositionButton(postavkeButton, 0.2, 0.6, 0.6, 0.2, ClientSize);
+    }
+
+    private void MainMenuView_Resize(object sender, EventArgs e) {
+      UtilityFunctions.PositionButton(igrajButton, 0.2, 0.2, 0.6, 0.2, ClientSize);
+      UtilityFunctions.PositionButton(postavkeButton, 0.2, 0.6, 0.6, 0.2, ClientSize);
+    }
+  }
+}
---
+++ BrojkeISlova/BrojkeISlova/UtilityFunctions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrojkeISlova {
+  static class UtilityFunctions {
+    public static Font GetFontForTextBoxHeight(int TextBoxHeight, Font OriginalFont) {
+      // What is the target size of the textbox?
+      float desiredheight = (float)TextBoxHeight;
+
+      // Set the font from the existing TextBox font.
+      // We use the fnt = new Font(...) method so we can ensure that
+      //  we're setting the GraphicsUnit to Pixels.  This avoids all
+      //  the DPI conversions between point & pixel.
+      Font fnt = new Font(OriginalFont.FontFamily,
+                          OriginalFont.Size,
+                          OriginalFont.Style,
+                          GraphicsUnit.Pixel);
+
+      // TextBoxes never size below 8 pixels. This consists of the
+      // 4 pixels above & 3 below of whitespace, and 1 pixel line of
+      // greeked text.
+      if (desiredheight < 8)
+        desiredheight = 8;
+
+      // Determine the Em sizes of the font and font line spacing
+      // These values are constant for each font at the given font style.
+      // and screen DPI.
+      float FontEmSize = fnt.FontFamily.GetEmHeight(fnt.Style);
+      float FontLineSpacing = fnt.FontFamily.GetLineSpacing(fnt.Style);
+
+      // emSize is the target font size.  TextBoxes have a total of
+      // 7 pixels above and below the FontHeight of the font.
+      float emSize = (desiredheight - 7) * FontEmSize / FontLineSpacing;
+
+      // Create the font, with the proper size.
+      fnt = new Font(fnt.FontFamily, emSize, fnt.Style, GraphicsUnit.Pixel);
+
+      return fnt;
+    }
+    public static void PositionButton(Button b, double left, double top, double width, double height, Size size ) {
+      b.Left = (int)(size.Width * left);
+      b.Top = (int)(size.Height * top);
+      b.Width = (int)(size.Width * width);
+      b.Height = (int)(size.Height * height);
+    }
+  }
+}
