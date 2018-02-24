28e047fae20c38338ea7a8f277f0a9f62bcf3797
Zeljko Djuric <zdjuric@yandex.com>
2018-02-24 15:31:13 +0100

    Added items to brojke view.

--- BrojkeISlova/BrojkeISlova/BrojkeView.Designer.cs
+++ BrojkeISlova/BrojkeISlova/BrojkeView.Designer.cs
@@ -24,10 +24,97 @@
     /// </summary>
     private void InitializeComponent() {
       this.components = new System.ComponentModel.Container();
+      this.gotovButton = new System.Windows.Forms.Button();
+      this.stopButton = new System.Windows.Forms.Button();
+      this.remainingTimeProgressBar = new System.Windows.Forms.ProgressBar();
+      this.rjesenjeTextBox = new System.Windows.Forms.TextBox();
+      this.timer = new System.Windows.Forms.Timer(this.components);
+      this.trazeniBrojTextBox = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // gotovButton
+      // 
+      this.gotovButton.Location = new System.Drawing.Point(908, 371);
+      this.gotovButton.Name = "gotovButton";
+      this.gotovButton.Size = new System.Drawing.Size(199, 62);
+      this.gotovButton.TabIndex = 9;
+      this.gotovButton.Text = "GOTOVO";
+      this.gotovButton.UseVisualStyleBackColor = true;
+      this.gotovButton.Click += new System.EventHandler(this.gotovButton_Click);
+      // 
+      // stopButton
+      // 
+      this.stopButton.Location = new System.Drawing.Point(-1, 449);
+      this.stopButton.Name = "stopButton";
+      this.stopButton.Size = new System.Drawing.Size(1122, 119);
+      this.stopButton.TabIndex = 8;
+      this.stopButton.Text = "STOP";
+      this.stopButton.UseVisualStyleBackColor = true;
+      this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+      // 
+      // remainingTimeProgressBar
+      // 
+      this.remainingTimeProgressBar.Location = new System.Drawing.Point(130, 242);
+      this.remainingTimeProgressBar.MarqueeAnimationSpeed = 0;
+      this.remainingTimeProgressBar.Maximum = 1200;
+      this.remainingTimeProgressBar.Name = "remainingTimeProgressBar";
+      this.remainingTimeProgressBar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+      this.remainingTimeProgressBar.Size = new System.Drawing.Size(772, 60);
+      this.remainingTimeProgressBar.Step = 100;
+      this.remainingTimeProgressBar.TabIndex = 7;
+      this.remainingTimeProgressBar.Value = 1200;
+      // 
+      // rjesenjeTextBox
+      // 
+      this.rjesenjeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      this.rjesenjeTextBox.Location = new System.Drawing.Point(-1, 371);
+      this.rjesenjeTextBox.Name = "rjesenjeTextBox";
+      this.rjesenjeTextBox.Size = new System.Drawing.Size(903, 62);
+      this.rjesenjeTextBox.TabIndex = 6;
+      this.rjesenjeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      // 
+      // timer
+      // 
+      this.timer.Enabled = true;
+      this.timer.Interval = 50;
+      this.timer.Tick += new System.EventHandler(this.timer_Tick);
+      // 
+      // trazeniBrojTextBox
+      // 
+      this.trazeniBrojTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      this.trazeniBrojTextBox.Location = new System.Drawing.Point(425, 71);
+      this.trazeniBrojTextBox.Name = "trazeniBrojTextBox";
+      this.trazeniBrojTextBox.Size = new System.Drawing.Size(236, 62);
+      this.trazeniBrojTextBox.TabIndex = 10;
+      this.trazeniBrojTextBox.Text = "1000";
+      this.trazeniBrojTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      // 
+      // BrojkeView
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1120, 569);
+      this.Controls.Add(this.trazeniBrojTextBox);
+      this.Controls.Add(this.gotovButton);
+      this.Controls.Add(this.stopButton);
+      this.Controls.Add(this.remainingTimeProgressBar);
+      this.Controls.Add(this.rjesenjeTextBox);
+      this.Name = "BrojkeView";
       this.Text = "BrojkeView";
+      this.Load += new System.EventHandler(this.BrojkeView_Load);
+      this.Resize += new System.EventHandler(this.BrojkeView_Resize);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Button gotovButton;
+    private System.Windows.Forms.Button stopButton;
+    private System.Windows.Forms.ProgressBar remainingTimeProgressBar;
+    private System.Windows.Forms.TextBox rjesenjeTextBox;
+    private System.Windows.Forms.Timer timer;
+    private System.Windows.Forms.TextBox trazeniBrojTextBox;
   }
 }
--- BrojkeISlova/BrojkeISlova/BrojkeView.cs
+++ BrojkeISlova/BrojkeISlova/BrojkeView.cs
@@ -10,8 +10,122 @@ using System.Windows.Forms;
 
 namespace BrojkeISlova {
   public partial class BrojkeView : Form {
-    public BrojkeView() {
+    int brojBrojki;
+    String dopusteneOperacije;
+    Random random = new Random();
+    TextBox[] brojke = new TextBox[15];
+    String[] moguceBrojke = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "25", "35", "50", "75" };
+    int trenutnaBrojka = -1;
+    int offset = 2;
+    //int[] brojPojavljivanjaZadanihSlova = new int[30];
+
+    public BrojkeView(int brojBrojki = 10, string dopusteneOperacije = "+-*/^") {
       InitializeComponent();
+      this.brojBrojki = brojBrojki;
+      this.dopusteneOperacije = dopusteneOperacije;
+      WindowState = FormWindowState.Maximized;
+    }
+
+    private void BrojkeView_Load(object sender, EventArgs e) {
+      for (int i = 0; i < brojBrojki; i++) {
+        brojke[i] = new TextBox();
+        brojke[i].TextAlign = HorizontalAlignment.Center;
+        Controls.Add(brojke[i]);
+      }
+      ResizeBrojki();
+      ResizeRjesenje();
+      ResizeRemainingTime();
+      ResizeGotovButton();
+      ResizeStopButton();
+      ResizeTrazeniBrojButton();
+    }
+
+    private void ResizeBrojki() {
+      int top = (int)(0.47 * ClientSize.Height);
+      int width = (int)(ClientSize.Width - 11 * offset) / 10;
+      int height = (int)(0.15 * ClientSize.Height);
+      int left = (int)(ClientSize.Width - (brojBrojki * width + (brojBrojki - 1) * offset)) / 2;
+      for (int i = 0; i < brojBrojki; i++) {
+        left += offset;
+        UtilityFunctions.PositionTextBox(brojke[i], left, top, width, height);
+        left += width;
+      }
+    }
+
+    private void BrojkeView_Resize(object sender, EventArgs e) {
+      ResizeBrojki();
+      ResizeRjesenje();
+      ResizeRemainingTime();
+      ResizeGotovButton();
+      ResizeStopButton();
+      ResizeTrazeniBrojButton();
+    }
+
+    private void ResizeRjesenje() {
+      int left = 10;
+      int top = (int)(0.78 * ClientSize.Height);
+      int width = (int)(0.7 * ClientSize.Width) - 2 * 10;
+      int height = (int)(0.1 * ClientSize.Height);
+      UtilityFunctions.PositionTextBox(rjesenjeTextBox, left, top, width, height);
+    }
+
+    private void ResizeGotovButton() {
+      UtilityFunctions.PositionButton(gotovButton, 0.71, 0.78, 0.28, 0.1, ClientSize);
+    }
+
+    private void ResizeStopButton() {
+      UtilityFunctions.PositionButton(stopButton, 0, 0.9, 1, 0.1, ClientSize);
+    }
+
+    private void ResizeRemainingTime() {
+      UtilityFunctions.PositionProgressBar(remainingTimeProgressBar, 0.2, 0.71, 0.6, 0.05, ClientSize);
+    }
+
+    private void ResizeTrazeniBrojButton() {
+      int top = (int)(0.19 * ClientSize.Height);
+      int width = (int)(0.3 * ClientSize.Width);
+      int height = (int)(0.15 * ClientSize.Height);
+      int left = (ClientSize.Width - width) / 2;
+      UtilityFunctions.PositionTextBox(trazeniBrojTextBox, left, top, width, height);
+    }
+
+    private void timer_Tick(object sender, EventArgs e) {
+      if (trenutnaBrojka == -1) {
+        stopButton.Focus();
+        trazeniBrojTextBox.Text = random.Next(1, 1001).ToString();
+      }
+      else if (trenutnaBrojka < brojBrojki) {
+        stopButton.Focus();
+        brojke[trenutnaBrojka].Text = moguceBrojke[random.Next(0, 24)];
+      }
+      else {
+        if (remainingTimeProgressBar.Value - 1 >= 0) {
+          remainingTimeProgressBar.Value -= 1;
+        }
+        else {
+          Kraj();
+        }
+      }
+    }
+
+    private void stopButton_Click(object sender, EventArgs e) {
+      ++trenutnaBrojka;
+    }
+
+    private void Kraj() {
+      timer.Enabled = false;
+      remainingTimeProgressBar.Value = 0;
+      if (true) {
+        MessageBox.Show("Vaše rješenje: " + rjesenjeTextBox.Text + "\n" + "Najbolje rješenje: " + "placeholder");
+      }
+      else {
+        MessageBox.Show("Neispravan unos.\n" + "Najbolje rješenje: " + "placeholder");
+      }
+      this.Close();
+    }
+
+    private void gotovButton_Click(object sender, EventArgs e) {
+      Kraj();
     }
   }
 }
